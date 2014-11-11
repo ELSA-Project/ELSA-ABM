ffa1e58ab08ed64da3145aca243346868063c582
GGurtner <gurtner.gerald@gmail.com>
2014-11-11 15:53:03 +0100

    Same as previous, forgot to commit some files.

---
+++ src/engine/compile.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+if __name__ == '__main__':
+	if len(sys.argv)==2 and sys.argv[1] == '--debug':
+		#os.system("clang *.c -g3 -fsanitize=address -o ElsaABM.so -lm -shared -fno-omit-frame-pointer -fPIC -I/usr/include/python2.7/ -lpython2.7")
+		command ="clang *.c -g3 -fsanitize=address -o ElsaABM.so -lm "
+	else:
+		command = "gcc mABM.c main.c mQuery.c mSector.c mTest.c mUtility.c -o ElsaABM -lm"
+		
+	os.system(command)
+	print "Executed this command:", command
---
+++ src/engine/test_execution.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+#Execution for testing purposes...
+
+./ElsaABM /home/earendil/Documents/ELSA/ABM_Tactic/ABM_FINAL/trajectories/M1/inputABM_n-10_Eff-0.975743921611_Nf-1500.dat /home/earendil/Documents/ELSA/ABM_Tactic/ABM_FINAL/results/output.dat
