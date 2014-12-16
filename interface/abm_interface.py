3c280664024024a5a6423db02ba916fbd23f9469
GGurtner <gurtner.gerald@gmail.com>
2014-12-16 18:30:15 +0100

    Previous commit continued

--- interface/abm_interface.py
+++ interface/abm_interface.py
@@ -12,7 +12,7 @@ import pickle
 
 from abm_tactical import *
 from abm_strategic import *
-from abm_strategic.utilities import draw_network_map
+from abm_strategic.utilities import draw_network_map, select_interesting_navpoints, OD
 
 
 if 1:
@@ -21,7 +21,7 @@ if 1:
     #see_ = 15 #==> the one I used for new, old_good, etc.. 
     # see=2
     print "===================================="
-    print "USING SEED", see_
+    print "          USING SEED", see_
     print "===================================="
     np.random.seed(see_)
 
@@ -30,47 +30,37 @@ def do_plop():
 	#from abm_strategic.paras_G import paras_G
 	#G = prepare_hybrid_network(paras_G)
 
-
+	# Choice of the network
 	sys.path.insert(1, '../abm_strategic')
 	with open("../networks/D_N44_nairports22_cap_constant_C5_w_coords_Nfp2.pic", 'r') as f:
 		G = pickle.load(f)
 
-	def get_coords(nvp):
-		return G.G_nav.node[nvp]['coord']
+	# Choice of the trajectories
+	trajectories = generate_traffic(G, paras_file='../abm_strategic/paras.py', save_file=None, simple_setup=True, 
+		starting_date=[2010, 6, 5, 10, 0, 0], coordinates=False, ACtot=100)
 
-	def add_node(trajs, G, coords, f, p):
-		new_node = len(G.nodes())
-		G.add_node(new_node, coord = coords)
+	# Make groups
+	n_best = select_interesting_navpoints(G, OD = OD(trajectories), N_per_sector = 1) # Selecting points with highest betweenness centrality within each sector
+	n_best = [n for sec, points in n_best.items() for n in points]
+	# groups = {}
+	# for n in G.G_nav.nodes():
+	# 	gg = np.random.choice(["A", "B"])
+	# 	groups[gg] = groups.get(gg, []) + [n]
+	# probabilities = {"A":0.7, "B":0.3}
 
-		#trajs[f].remove(n)
-		trajs[f][p] = new_node
-
-		return new_node, trajs, G
-
-	
-	trajectories = generate_traffic(G, paras_file = '../abm_strategic/paras.py', save_file = None,  simple_setup=True, 
-		starting_date = [2010, 6, 5, 10, 0, 0], coordinates = False, ACtot=100)
-
-
-	def d((n1, n2)):
-		p1 = get_coords(n1)
-		p2 = get_coords(n2)
-		return np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
-
-	groups = {}
+	groups = {"C":[], "N":[]} # C for "critical", N for "normal"
 	for n in G.G_nav.nodes():
-		gg = np.random.choice(["A", "B"])
-		groups[gg] = groups.get(gg, []) + [n]
+		if n in n_best:
+			groups["C"].append(n)
+		else:
+			groups["N"].append(n)
+	probabilities = {"C":0.001, "N":0.999}
 
-	probabilities = {"A":0.7, "B":0.3}
+	draw_network_map(G.G_nav, title='Network map', trajectories=trajectories, rep='./', airports=False, load=False, generated=True, add_to_title='', polygons=G.polygons.values(), numbers=False, show=False)
 
-	draw_network_map(G.G_nav, title='Network map', trajectories=trajectories, rep='./', airports=False, load=False, generated=True, add_to_title='', polygons=[], numbers=False, show=False)
+	traj_eff = rectificate_trajectories_network(trajectories, 0.995, G, groups=groups, probabilities=probabilities, remove_nodes = True)
 
-	traj_eff = rectificate_trajectories(trajectories, 0.999, dist_func = d, add_node_func = add_node, coords_func =  get_coords,
-		 G = G.G_nav, groups = groups, probabilities = probabilities, remove_nodes = True)
-
-	draw_network_map(G.G_nav, title='Network map', trajectories=traj_eff, rep='./', airports=False, load=False, generated=True, add_to_title='', polygons=[], numbers=False, show=True)
-    
+	draw_network_map(G.G_nav, title='Network map', trajectories=traj_eff, rep='./', airports=False, load=False, generated=True, add_to_title='', polygons=G.polygons.values(), numbers=False, show=True)
 
 
 def do_ABM_tactical(input_file, output_file):
