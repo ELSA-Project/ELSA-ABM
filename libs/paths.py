a4ec5fed12d3bf0a8a94573cf45bd6874a635064
GGurtner <gurtner.gerald@gmail.com>
2015-01-08 15:18:43 +0100

    Cleaning

---
+++ libs/paths.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Paths for utilities or libraries.
+"""
+from os.path import join, split, dirname
+
+path_ksp = '/home/earendil/Programmes/YenKSP-master'
+path_codes = '/home/earendil/Documents/ELSA/Codes/'
+path_utilities = '/home/earendil/Documents/ELSA/Utilities/'
+path_modules = join(split(dirname(__file__))[0], 'libs')
