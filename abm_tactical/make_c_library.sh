64db65fb4c2ef39446dd61b9292334021a5865b7
GGurtner <gerald.gurtner@sns.it>
2015-02-05 18:57:49 +0100

    Added folder "tests" + script for bounds generation

--- abm_tactical/make_c_library.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-swig -python ABMtactic.i
-gcc -fpic -c -I/usr/include/python2.7 -I/usr/lib/python2.7/config/ *.c 
-#gcc -shared main.o ABMtactic_wrap.o -lm -o _ABMtactic.so
-gcc -shared *.o -lm -o _ABMtactic.so
-
