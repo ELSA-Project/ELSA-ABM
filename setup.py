a27ad3de60679ff659092cec2889dee226acae10
GGurtner <gerald.gurtner@sns.it>
2015-05-11 17:17:05 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Setup script for the model. You can safely run this several times if you do not mess up too much
+with libs/paths.py.
+"""
+
+import os
+
+if __name__ == '__main__':
+	main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+	print "Please type the path of the main directory for results. The path can be relative."
+	print "It will be created if it does not exist."
+	ans = raw_input("Leave blank (press enter) to use the default, which is ../results\n")
+
+	print "Writing libs/paths.py file..."
+	if ans=='':
+		ans = '../results'
+		
+	result_dir = os.path.abspath(ans)
+
+	with open(os.path.join(main_dir, 'libs/paths.py')) as f:
+		lines = f.readlines()
+
+	new_lines = []
+	found = False
+	for l in lines:
+		if 'result_dir' in l and l[0]!='#':
+			new_lines.append('result_dir = "' + result_dir + '"')
+			found = True
+		else:
+			new_lines.append(l)
+
+	if not found:
+		new_lines.append('')
+		new_lines.append('result_dir = "' + result_dir + '"')
+
+	with open(os.path.join(main_dir, 'libs/paths.py'), 'w') as f:
+		for line in new_lines:
+			f.write(line)
+
+	os.system('mkdir -p ' + result_dir)
+
+	print
+	print "Writing C code and making wrapper..."
+	os.system("cd abm_tactical && ./compile.py")
+
+	print
+	print "Writing Documentation..."
+	os.system("./make_doc.py")
+
+
